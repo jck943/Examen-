b279b75231257f1e11864a44011cc6f223272fc6
Jacobo <correo>
2025-03-12 19:57:49 +0100

    Examen 2º commit

--- Examen.py
+++ Examen.py
@@ -1,11 +1,14 @@
 def buscarPalabra(objetivo, palabras):
     if objetivo in palabras:
-        print("True")
+        return True
     else:
-        print("False")
-
+        return False
+ 
 def imprimirListaInversa(lista):
-    
+    nueva_lista = []
+    for i in range(len(lista) - 1, -1, -1):
+        nueva_lista = lista[i]
+        print("- " + nueva_lista)
 
 nombres = ["Mengano", "Fulano", "Zutano", "Perantano"]
 edades = {
@@ -14,6 +17,14 @@ edades = {
     "Zutano": 50,
     "Perantano": 75
 }
-
-objetivo = input("Buscar nombre: ")
-buscarPalabra(objetivo, nombres)
+imprimirListaInversa(nombres)
+while True:
+    print()
+    objetivo = input("Buscar nombre: ")
+    if objetivo == "exit":
+        print("\nFIN DEL PROGRAMA")
+        break
+    elif buscarPalabra(objetivo, nombres) == True:
+        print(objetivo + " tiene " + str(edades[objetivo]) + " años")
+    else:
+        print("El nombre no existe...")
